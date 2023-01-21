82f6c0078a32d1696150e9e46ac619c932009ae0
vidura-chahuranga <vidurachathuranga2020@gmail.com>
2023-01-21 23:48:25 +0530

    Created Mobile NavBar

--- frontend/src/scenes/navbar/index.jsx
+++ frontend/src/scenes/navbar/index.jsx
@@ -1,7 +1,76 @@
-const NavBar = ()=>{
-    return(
-        <div>NavBar</div>
-    )
-}
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notification,
+  Help,
+  Menu,
+  Close,
+  Search,
+} from "@mui/icons-material";
+import { setMode, setLogout } from "../../state/index.js";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../../components/FlexBetween";
+import { useDispatch, useSelector } from "react-redux";
+
+const NavBar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(falase);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width : 1000px)");
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.neutral.background.default;
+  const primaryLight = theme.palette.neutral.primary.light;
+  const alt = theme.palette.neutral.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+
+  return (
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem,2rem, 2.25rem)"
+          color="primary"
+          onClick={() => navigate("/home")}
+          sx={{ "&:hover": { color: primaryLight, cursor: pointer } }}
+        >
+          SocialMedia
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="9px"
+            gap="3rem"
+            paddding="0.1rem 1.5rem"
+          >
+            {" "}
+            <InputBase placeholder="Search... " />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
 
 export default NavBar;
--- frontend/src/state/index.js
+++ frontend/src/state/index.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-//we can use this states anywhere
+//we can use states anywhere
 const initialState = {
-  mode: "light", // represent dark mode and light mode
+  mode: "light",//represent dart light theme
   user: null,
   token: null,
   posts: [],
@@ -12,7 +12,6 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    //functions use to modify above global variables
     setMode: (state) => {
       state.mode = state.mode === "light" ? "dark" : "light";
     },
@@ -21,28 +20,29 @@ export const authSlice = createSlice({
       state.token = action.payload.token;
     },
     setLogout: (state) => {
-      (state.user = null), (state.token = null);
+      state.user = null;
+      state.token = null;
     },
     setFriends: (state, action) => {
       if (state.user) {
         state.user.friends = action.payload.friends;
       } else {
-        console.log("user friends non-existent :(");
+        console.error("user friends non-existent :(");
       }
     },
     setPosts: (state, action) => {
       state.posts = action.payload.posts;
     },
     setPost: (state, action) => {
-      const updatePosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) return action.payload.post;
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
         return post;
       });
-      state.posts = updatePosts;
+      state.posts = updatedPosts;
     },
   },
 });
 
-export const { setMode, esstLogin, setLogout, setFriends, setPosts, setPost } =
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
   authSlice.actions;
 export default authSlice.reducer;
