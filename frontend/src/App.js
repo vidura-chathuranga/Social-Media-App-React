f7e989513da7b8943be3da94f7df844ee85732a7
vidura-chahuranga <vidurachathuranga2020@gmail.com>
2023-01-21 20:37:14 +0530

    initialize redux toolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/profile/:userId" element={<ProfilePage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/scenes/homepage/index.jsx
@@ -0,0 +1,7 @@
+const HomePage = ()=>{
+    return(
+        <div>homePage</div>
+    )
+}
+
+export default HomePage;
